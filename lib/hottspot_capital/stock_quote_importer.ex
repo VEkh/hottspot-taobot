122f6784fdcdb37d0e1725ce464bc03f76691a4e
Vidal Ekechukwu <vekechuk@gmail.com>
2019-07-17 19:01:19 +0000

    Import most recent stock quote for a single symbol

--- lib/hottspot_capital/stock_quote_importer.ex
+++ lib/hottspot_capital/stock_quote_importer.ex
@@ -2,6 +2,24 @@ defmodule HottspotCapital.StockQuoteImporter do
   alias HottspotCapital.IexApiClient
   alias HottspotCapital.StockQuote
 
+  def import(symbol) do
+    fetched_stock_quote = IexApiClient.fetch_stock_quote(symbol)
+
+    case fetched_stock_quote do
+      %IexApiClient.StockQuote{} ->
+        {:ok, stock_quote} =
+          fetched_stock_quote
+          |> Map.from_struct()
+          |> StockQuote.changeset()
+          |> StockQuote.upsert()
+
+        stock_quote
+
+      resp ->
+        resp
+    end
+  end
+
   def import_historical(symbol, years: years) do
     fetched_historical_quotes = IexApiClient.fetch_historical_stock_quotes(symbol, years: years)
 
