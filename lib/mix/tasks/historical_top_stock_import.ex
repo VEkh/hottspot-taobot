5e795e49a20e917391b37634b362e13806cdbedf
Vidal Ekechukwu <vekechuk@gmail.com>
2019-09-04 05:59:23 +0000

    Fix: Mix task didn't start hottspot_capital app

--- lib/mix/tasks/historical_top_stock_import.ex
+++ lib/mix/tasks/historical_top_stock_import.ex
@@ -28,21 +28,27 @@ defmodule Mix.Tasks.HistoricalTopStockImport do
 
   @shortdoc "Import historical stock quotes for top n companies"
   def run(options) do
+    Application.ensure_all_started(:hottspot_capital)
+
     %{"companies_limit" => companies_limit} = parse_options(options)
 
     Company.get_largest(companies_limit)
     |> Enum.flat_map(fn
       %{symbol: symbol} ->
         range = StockQuote.missing_data_range(symbol)
-        log_import(range: range, symbol: symbol)
+        log_import(env: Mix.env(), range: range, symbol: symbol)
         Importer.import_historical(range: range, symbol: symbol)
     end)
   end
 
-  defp log_import(range: range, symbol: symbol) do
-    if Mix.env() != :test do
-      IO.puts("Importing #{symbol}'s stock quotes for the past #{range}")
+  defp log_import(env: :test, range: _, symbol: _), do: nil
+
+  defp log_import(env: _, range: range, symbol: symbol) do
+    case range do
+      :no_missing_data -> "#{symbol} is not missing any data"
+      _ -> "Importing #{symbol}'s stock quotes for the past #{range}"
     end
+    |> IO.puts()
   end
 
   defp parse_options(options) do
