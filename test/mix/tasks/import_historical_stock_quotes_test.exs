68e3953de2abc443c3c0d61ae623913b32afff90
Vidal Ekechukwu <vekechuk@gmail.com>
2020-01-02 03:08:18 +0000

    Import historical stocks of all companies
    * Company rankings change over time. If we only import historical quotes
    of the top companies, when a company falls from the top 200 but it was
    in a top company's basket, then it will no longer be included in the
    basket. This can give an inaccurate representation of the reference
    company's movement.

--- test/mix/tasks/import_historical_stock_quotes_test.exs
+++ test/mix/tasks/import_historical_stock_quotes_test.exs
@@ -1,11 +1,11 @@
-defmodule Mix.Tasks.ImportHistoricalTopStocksTest do
+defmodule Mix.Tasks.ImportHistoricalStockQuotesTest do
   use HottspotCapital.Test.DataCase
   use HottspotCapital.Test.MockCase
 
   alias HottspotCapital.Repo
   alias HottspotCapital.StockQuote
   alias HottspotCapital.Test.Factory
-  alias Mix.Tasks.ImportHistoricalTopStocks
+  alias Mix.Tasks.ImportHistoricalStockQuotes
 
   test "imports historical stocks from top n companies (default options)" do
     [
@@ -17,31 +17,12 @@ defmodule Mix.Tasks.ImportHistoricalTopStocksTest do
     ]
     |> Enum.each(&Factory.create_company/1)
 
-    ImportHistoricalTopStocks.run([])
+    ImportHistoricalStockQuotes.run([])
 
     assert %{
              "AMZN" => [2013, 2014, 2015, 2016, 2017, 2018],
              "FB" => [2013, 2014, 2015, 2016, 2017, 2018],
              "GOOG" => [2013, 2014, 2015, 2016, 2017, 2018],
-             "HOTT" => [2013, 2014, 2015, 2016, 2017, 2018],
-             "MSFT" => [2013, 2014, 2015, 2016, 2017, 2018]
-           } = get_imported_quotes()
-  end
-
-  test "accepts companies_limit argument" do
-    [
-      %{market_cap: 8, symbol: "AMZN"},
-      %{market_cap: 7, symbol: "FB"},
-      %{market_cap: 6, symbol: "GOOG"},
-      %{market_cap: 10, symbol: "HOTT"},
-      %{market_cap: 9, symbol: "MSFT"}
-    ]
-    |> Enum.each(&Factory.create_company/1)
-
-    ImportHistoricalTopStocks.run(["--companies_limit=3"])
-
-    assert %{
-             "AMZN" => [2013, 2014, 2015, 2016, 2017, 2018],
              "HOTT" => [2013, 2014, 2015, 2016, 2017, 2018],
              "MSFT" => [2013, 2014, 2015, 2016, 2017, 2018]
            } = get_imported_quotes()
