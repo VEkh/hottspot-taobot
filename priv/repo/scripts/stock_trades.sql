e1d450c5cea187a0fdaaf97e5d282ed283cf53d8
Vidal Ekechukwu <vekechuk@gmail.com>
2020-01-13 18:38:53 +0000

    Create and associate stock buys, sells & positions

--- priv/repo/scripts/stock_trades.sql
+++ priv/repo/scripts/stock_trades.sql
@@ -1,2 +1,11 @@
-insert into stock_trades (action, filled_at, price, symbol, volume) values
-  ('buy', '2020-01-01T00:00 EST', 10000, 'AAPL', 10)
+insert into stock_buys (executed_at, price, symbol, volume) values
+  ('2020-01-01T00:00 EST', 10000, 'AAPL', 10)
+;
+
+insert into stock_sells (executed_at, price, symbol, volume) values
+  ('2020-01-02T00:00 EST', 10000, 'AAPL', 10)
+;
+
+insert into stock_positions (stock_buy_id, stock_sell_id) values
+  (1, 1)
+;
