fed9190b73c4134b6e6b3bbc19380a751f4f5743
Vidal Ekechukwu <vekechuk@gmail.com>
2020-11-13 21:01:36 +0000

    Write TDAmeritrade request response to file

--- cpp/sandbox/src/td_ameritrade/td_ameritrade_client.cpp
+++ cpp/sandbox/src/td_ameritrade/td_ameritrade_client.cpp
@@ -1,7 +1,9 @@
-#include "td_ameritrade_client.h"
-#include "curl_client.cpp"    // CurlClient
-#include "utils/debugger.cpp" // utils::debugger::inspect
-#include <string>             // std::string
+#include "td_ameritrade_client.h" // TOKENS_PATH, json_parser
+#include "curl_client.cpp"        // CurlClient
+#include "utils/debugger.cpp"     // utils::debugger::inspect
+#include <fstream>  // std::ios::out, std::ios::trunc, std::ofstream
+#include <iostream> // std::cout
+#include <string>   // std::string
 
 // TODO
 void TdAmeritradeClient::get_acces_token() {
@@ -48,12 +50,12 @@ void TdAmeritradeClient::get_quote(std::string symbol) {
 
 void TdAmeritradeClient::refresh_token() {
   simdjson::dom::element client_config =
-      json_parser.load("./tmp/td_auth/credentials.json");
+      json_parser.load("./config/td_ameritrade/credentials.json");
 
   std::string client_id = std::string(client_config["client_id"]);
   std::string redirect_uri = std::string(client_config["redirect_uri"]);
 
-  simdjson::dom::element tokens = json_parser.load("./tmp/td_auth/tokens.json");
+  simdjson::dom::element tokens = json_parser.load(TOKENS_PATH);
   std::string refresh_token = std::string(tokens["refresh_token"]);
 
   CurlClient::props_t props = {
@@ -70,7 +72,27 @@ void TdAmeritradeClient::refresh_token() {
   };
 
   CurlClient curl_client(props);
-  curl_client.print_request();
   curl_client.request();
-  utils::debugger::inspect(curl_client.response.body);
+
+  simdjson::dom::element response =
+      json_parser.parse(curl_client.response.body);
+
+  if (!std::string(response["error"]).empty()) {
+    std::cout << response << std::endl;
+    return;
+  }
+
+  std::cout << "Successfully refreshed tokens: \n"
+            << curl_client.response.body << std::endl;
+
+  std::cout << "Writing to: " << TOKENS_PATH << std::endl;
+
+  write_response_to_file(curl_client.response.body, TOKENS_PATH);
+}
+
+void TdAmeritradeClient::write_response_to_file(std::string body,
+                                                const char *file_path) {
+  std::ofstream output_file(file_path, std::ios::out | std::ios::trunc);
+  output_file << body;
+  output_file.close();
 }
--- cpp/sandbox/src/td_ameritrade/td_ameritrade_client.h
+++ cpp/sandbox/src/td_ameritrade/td_ameritrade_client.h
@@ -1,12 +1,16 @@
 #if !defined(TD_AMERITRADE_CLIENT)
 #define TD_AMERITRADE_CLIENT
 
-#include "../../deps/simdjson/simdjson.cpp" // simdjson::dom::parser
-#include <string>                           // std::string
+#include "deps/simdjson/simdjson.cpp" // simdjson::dom::parser
+#include <string>                     // std::string
 
 class TdAmeritradeClient {
 private:
+  const char *TOKENS_PATH = "./config/td_ameritrade/tokens.json";
+
   simdjson::dom::parser json_parser;
+
+  void write_response_to_file(std::string, const char *);
 
 public:
   void get_acces_token();
