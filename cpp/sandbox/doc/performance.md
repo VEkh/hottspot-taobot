c7103267f9e3b8a39378bbe92c75152e550cc74a
Vidal Ekechukwu <vekechuk@gmail.com>
2021-01-30 22:41:12 +0000

    Trailing stop algorithm v0.3

--- cpp/sandbox/doc/performance.md
+++ cpp/sandbox/doc/performance.md
@@ -1,16 +1,41 @@
 # Entry Indicator Performance
 ## Wins
-### .15% Displacement
+### .15% Displacement | Trailing Stop Algorithm v0.1 | 1 sec inverval
 64/104 => 0.6153846153846154
 
-### .15% Displacement + 10 & 30-tick Velocities
+### .15% Displacement | 10 & 30-tick Velocities | Trailing Stop Algorithm v0.1 | 1 sec interval
 4/12 => 0.3333333333333
 
-### .15% Displacement + 10-tick Velocity
-26/42 => 0.6190476190476191 // 2021-01-28
+### .15% Displacement | 10-tick Velocity | Trailing Stop Algorithm v0.1 | 1 sec interval
+26/42 => 0.6190476190476191 (2021-01-28)
 
-### .15% Displacement + 10-tick Velocity + Trailing Stop Algorithm v0.2
-26/42 => 0.6190476190476191 // 2021-01-28
+### .15% Displacement | 10-tick Velocity | Trailing Stop Algorithm v0.2 | 1 sec interval (2021-01-29, am)
+6/17 (In the morning)
+
+### .15% Displacement | 10-tick Velocity | Trailing Stop Algorithm v0.2 | 1 min interval (2021-01-29, am)
+2/9
+
+### 15% Displacement | Trailing Stop Algorithm v0.2 | 500 ms interval (2021-01-29, pm)
+*Raw*
+18/30 => 0.6
+
+*Small Losses as Wins*
+25/30 => 0.8333333333333334
+
+*Max Potential Loss During Wins*
+This is the greates the price moved in the losing direction before ultimately winning. It counts
+[0.05, 1.71, 1.79, 0.25, 1.45, 0.14, 2.81\*, 0.19, 0.70, 0.68, 0.41, 3.71, 0.73, 0.61, 0.64, 4.02, 0.45, 0.32, 3.11, 1.38, 0.71, 0.34, 0.66, 1.70]
+
+*Max Max Potential Loss*
+4.02 == 0.5024999999999999% of $800
+
+*Avg Max Potential Loss*
+1.1899999999999997
+
+\* Potential loss of $2.81 resulted in one of biggest wins
+
+*Max Loss Magnitude*
+6.77
 
 ## Closes
 ### How often price reversal continue in reversal direction?
--- cpp/sandbox/src/etrade/straddle/set_trailing_stop_price.cpp
+++ cpp/sandbox/src/etrade/straddle/set_trailing_stop_price.cpp
@@ -7,18 +7,29 @@
 #include <iostream>        // std::cout, std::endl
 #include <math.h>          // INFINITY
 
-const double LOSS_TRAILING_STOP_RATIO = 0.002;
-const double SECURE_PROFIT_PRICE = 0.50;
+const double LOSS_TRAILING_STOP_RATIO = 0.005;
+const double SECURE_PROFIT_RATIO = 0.001;
 
 double compute_trailing_stop(const double open_execution_price,
                              const double profit,
                              const double ten_tick_velocity) {
-  if (profit <= SECURE_PROFIT_PRICE) {
+  const double secure_profit_price = SECURE_PROFIT_RATIO * open_execution_price;
+
+  if (profit <= secure_profit_price) {
     return LOSS_TRAILING_STOP_RATIO * open_execution_price;
   }
 
-  return log10(profit + 1 + SECURE_PROFIT_PRICE) *
-         (1 + (ten_tick_velocity * 100));
+  const double min_trailing_stop_ratio = SECURE_PROFIT_RATIO * 0.5;
+  const double min_trailing_top =
+      min_trailing_stop_ratio * open_execution_price;
+
+  const double x = profit;
+  const double x_shift = secure_profit_price - 1;
+  const double y_multiplier = 1 + ten_tick_velocity;
+
+  const double trailing_stop = log10(x + x_shift) * y_multiplier;
+
+  return std::max(trailing_stop, min_trailing_top);
 }
 
 void ETrade::Straddle::set_trailing_stop_price(order_t *close_order,
@@ -32,19 +43,22 @@ void ETrade::Straddle::set_trailing_stop_price(order_t *close_order,
   json current_quote = quotes.back();
 
   double current_price = current_quote["currentPrice"];
-
-  double trailing_stop =
-      compute_trailing_stop(execution_price, open_order->profit,
-                            speedometer.average_velocity(10).second);
-
+  double ten_tick_velocity = speedometer.average_velocity(10).second;
+  double trailing_stop;
   double trailing_stop_price;
 
   if (close_order->action == order_action_t::SELL) {
+    trailing_stop = compute_trailing_stop(execution_price, open_order->profit,
+                                          ten_tick_velocity);
+
     trailing_stop_price =
         close_order->stop_price != -INFINITY
             ? std::max(close_order->stop_price, (current_price - trailing_stop))
             : open_order->execution_price - trailing_stop;
   } else if (close_order->action == order_action_t::BUY_TO_COVER) {
+    trailing_stop = compute_trailing_stop(execution_price, open_order->profit,
+                                          -ten_tick_velocity);
+
     trailing_stop_price =
         close_order->stop_price != INFINITY
             ? std::min(close_order->stop_price, (current_price + trailing_stop))
