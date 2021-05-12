857e0d6ba258c2d3911c0468064cdda7c172d863
Vidal Ekechukwu <vekechuk@gmail.com>
2021-05-12 22:36:16 +0000

    Entry v0.22.2, Exit v0.35.2

--- cpp/sandbox/sandbox.cpp
+++ cpp/sandbox/sandbox.cpp
@@ -1,12 +1,7 @@
+#include "lib/websockets_client/websockets_client.cpp" // LibWebSocketsClient
 #include <iostream>
-#include <regex> // std::regex, std::regex_search
 
 int main() {
-  std::cout << (6.0 / 26) << std::endl;
-  std::cout << (12.0 / 37) << std::endl;
-  std::cout << (14.0 / 27) << std::endl;
-  std::cout << (12.0 / 38) << std::endl;
-  std::cout << (14.0 / 35) << std::endl;
-  std::cout << (14.0 / 42) << std::endl;
-  std::cout << (15.0 / 27) << std::endl;
+  WebSocketsClient client("wss://stream.data.alpaca.markets/v2/iex");
+  client.connect();
 }
