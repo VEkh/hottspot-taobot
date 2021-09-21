388b3383ade5789cf3fba407eb2f814867431fb8
Vidal Ekechukwu <vekechuk@gmail.com>
2021-09-21 21:17:55 +0000

    Re-add aggressive buy-sell ratio as entry signal

--- cpp/sandbox/src/etrade/tao_bot/should_open_position.cpp
+++ cpp/sandbox/src/etrade/tao_bot/should_open_position.cpp
@@ -53,7 +53,26 @@ bool ETrade::TaoBot::should_open_position() {
 }
 
 bool ETrade::TaoBot::should_open_position(order_action_t order_action) {
-  return candlesticks_in_direction(order_action);
+  const double long_door_threshold = 1.5;
+  const double short_door_threshold = 1.75;
+
+  if (candlesticks_in_direction(order_action)) {
+    return true;
+  }
+
+  switch (order_action) {
+  case order_action_t::BUY: {
+    return this->long_average_buy_sell_ratio >= long_door_threshold &&
+           this->short_average_buy_sell_ratio >= short_door_threshold;
+  }
+
+  case order_action_t::SELL_SHORT: {
+    return this->long_average_sell_buy_ratio >= long_door_threshold &&
+           this->short_average_sell_buy_ratio >= short_door_threshold;
+  }
+  }
+
+  return false;
 }
 
 #endif
