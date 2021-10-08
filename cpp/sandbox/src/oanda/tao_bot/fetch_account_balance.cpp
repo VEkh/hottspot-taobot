b8134aaa2b339bc5544b2776732041e0fb651cfe
Vidal Ekechukwu <vekechuk@gmail.com>
2021-10-08 01:36:17 +0000

    Oanda: Make leveraged trades

--- cpp/sandbox/src/oanda/tao_bot/fetch_account_balance.cpp
+++ cpp/sandbox/src/oanda/tao_bot/fetch_account_balance.cpp
@@ -9,12 +9,18 @@ Oanda::TaoBot::account_balance_t Oanda::TaoBot::fetch_account_balance() {
   json account_json = json::parse(this->api_client.fetch_account());
 
   const std::string balance = account_json["account"]["balance"];
-  const std::string margin_buying_power =
+  const std::string margin_rate_string = account_json["account"]["marginRate"];
+  const std::string margin_buying_power_string =
       account_json["account"]["marginAvailable"];
+
+  const double margin_rate = std::stod(margin_rate_string);
+
+  const double margin_buying_power =
+      std::stod(margin_buying_power_string) / margin_rate;
 
   return {
       .balance = std::stod(balance),
-      .margin_buying_power = std::stod(margin_buying_power),
+      .margin_buying_power = margin_buying_power,
   };
 }
 
