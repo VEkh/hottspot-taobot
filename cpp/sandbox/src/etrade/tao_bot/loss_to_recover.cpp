c21380d8af9c4cf29455d3d0436709f8668d9b00
Vidal Ekechukwu <vekechuk@gmail.com>
2021-11-03 22:43:08 +0000

    Loss to Recover borrows from the total deficit

--- cpp/sandbox/src/etrade/tao_bot/loss_to_recover.cpp
+++ cpp/sandbox/src/etrade/tao_bot/loss_to_recover.cpp
@@ -2,12 +2,25 @@
 #define ETRADE__TAO_BOT_loss_to_recover
 
 #include "tao_bot.h" // ETrade::TaoBot
+#include <algorithm> // std::min
 
 double ETrade::TaoBot::loss_to_recover() {
+  const double current_loss_streak_balance =
+      this->performance.current_loss_streak_balance;
+
   const double total_deficit =
       this->performance.current_balance - this->performance.max_balance;
 
-  return this->performance.current_loss_streak_balance;
+  if (current_loss_streak_balance) {
+    return current_loss_streak_balance;
+  }
+
+  if (total_deficit) {
+    const double borrowed_deficit = 50;
+    return std::min(total_deficit, borrowed_deficit);
+  }
+
+  return 0;
 }
 
 #endif
