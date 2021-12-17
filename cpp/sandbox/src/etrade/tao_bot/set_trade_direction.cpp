1a79dd28d384fbd2ce02153342ca1b018fd87a53
Vidal Ekechukwu <vekechuk@gmail.com>
2021-12-17 06:24:51 +0000

    Revert to alternating position after each trade

--- cpp/sandbox/src/etrade/tao_bot/set_trade_direction.cpp
+++ cpp/sandbox/src/etrade/tao_bot/set_trade_direction.cpp
@@ -1,12 +1,17 @@
 #ifndef ETRADE__TAO_BOT_set_trade_direction
 #define ETRADE__TAO_BOT_set_trade_direction
 
-#include "set_trade_direction.cpp" // set_trade_direction
-#include "flip_coin.cpp"           // flip_coin
-#include "iostream"                // std::count
-#include "stdio.h"                 // puts
-#include "tao_bot.h"               // ETrade::TaoBot, fmt
+#include "flip_coin.cpp" // flip_coin
+#include "tao_bot.h"     // ETrade::TaoBot, fmt
 
-void ETrade::TaoBot::set_trade_direction() { this->is_long_position = true; }
+void ETrade::TaoBot::set_trade_direction() {
+  if (!this->closed_positions.size()) {
+    this->is_long_position = flip_coin();
+
+    return;
+  }
+
+  this->is_long_position = !this->is_long_position;
+}
 
 #endif
