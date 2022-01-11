b4fc8077a766f268cac44e5fe237698409a82267
Vidal Ekechukwu <vekechuk@gmail.com>
2022-01-11 22:48:17 +0000

    Oanda catches parse quote error

--- cpp/sandbox/src/alpaca/tao_bot/fetch_quote.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/fetch_quote.cpp
@@ -1,14 +1,24 @@
 #ifndef ALPACA__TAO_BOT_fetch_quote
 #define ALPACA__TAO_BOT_fetch_quote
 
+#include "deps.cpp"  // nlohmann
 #include "tao_bot.h" // Alpaca::TaoBot, quote_t
 #include <string>    // std::string
 
 void Alpaca::TaoBot::fetch_quote() {
-  const std::string quote_string = this->api_client.fetch_quote(this->symbol);
-  const quote_t current_quote = this->api_client.parse_quote(quote_string);
+  try {
+    const std::string quote_string = this->api_client.fetch_quote(this->symbol);
+    const quote_t current_quote = this->api_client.parse_quote(quote_string);
 
-  this->quotes.push_back(current_quote);
+    this->quotes.push_back(current_quote);
+  } catch (nlohmann::detail::type_error &) {
+    std::string error_message = Formatted::error_message(
+        std::string("ALPACA__TAO_BOT_fetch_quote error when parsing"));
+
+    std::cout << error_message << fmt.reset << std::endl;
+
+    return fetch_quote();
+  }
 }
 
 #endif
