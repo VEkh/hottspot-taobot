69991f8707b3e6e8d8ccb45867d0e982dee8aab8
Vidal Ekechukwu <vekechuk@gmail.com>
2022-01-13 19:18:54 +0000

    Increase Oanda's position target movement
    2.0x spread led to 18 consecutive losses for EUR_JPY or USD_JPY (I can't
    remember)

--- cpp/sandbox/src/oanda/tao_bot/convert_price.cpp
+++ cpp/sandbox/src/oanda/tao_bot/convert_price.cpp
@@ -18,12 +18,6 @@ double Oanda::TaoBot::convert_price(const double price, const std::string from,
   const quote_t exchange_rate_quote =
       this->api_client.parse_quote(quote_string);
 
-  std::cout << fmt.bold << fmt.cyan;
-  printf("From: %s @ %.5f • To: %s @ %.5f • Exchange Rate Symbol: %s\n",
-         from.c_str(), price, to.c_str(), exchange_rate_quote.price,
-         exchange_rate_symbol.c_str());
-  std::cout << fmt.reset << std::endl;
-
   return exchange_rate_quote.price * price;
 }
 
