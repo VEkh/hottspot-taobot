122057c32f31ddc826f5f56f3b6e2047c3a24d41
Vidal Ekechukwu <vekechuk@gmail.com>
2022-03-12 02:54:22 +0000

    Close profitable position during low liquidity

--- cpp/sandbox/src/oanda/tao_bot/should_close_position.cpp
+++ cpp/sandbox/src/oanda/tao_bot/should_close_position.cpp
@@ -32,14 +32,14 @@ bool Oanda::TaoBot::should_close_position() {
 
   this->exit_prices = build_exit_prices();
 
-  if (current_spread() > spread_limit()) {
-    return false;
-  }
-
   if (this->open_order.max_profit >= this->exit_prices.min_profit &&
       this->open_order.profit >= this->exit_prices.lower_secure_profit &&
       this->open_order.profit <= this->exit_prices.upper_secure_profit) {
     return true;
+  }
+
+  if (current_spread() > spread_limit()) {
+    return false;
   }
 
   if (this->open_order.profit <= exit_prices.max_loss) {
