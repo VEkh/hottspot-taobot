8c190b694df078282a79ed75d07a9b7169859d21
Vidal Ekechukwu <vekechuk@gmail.com>
2022-08-20 19:05:44 +0000

    🦙 feat: Return logging includes NASDAQ and S&P500

--- cpp/sandbox/src/alpaca/returns.cpp
+++ cpp/sandbox/src/alpaca/returns.cpp
@@ -41,29 +41,34 @@ void log() {
 
   json returns = load();
   json all_returns = returns["all"];
-  json qqq_returns = returns["qqq"];
+  json nasdaq_returns = returns["nasdaq"];
+  json sp500_returns = returns["sp500"];
 
   bool is_loss_streak_broken = false;
   bool is_week_total_complete = false;
   bool is_win_streak_broken = false;
   double grand_total = 0.00;
   double avg_profit_ratio = 0.00;
-  double avg_qqq_profit_ratio = 0.00;
+  double avg_nasdaq_profit_ratio = 0.00;
+  double avg_sp500_profit_ratio = 0.00;
   double week_total = 0.00;
   int all_returns_length = all_returns.size();
   int consecutive_losses = 0;
   int consecutive_wins = 0;
-  int qqq_returns_length = qqq_returns.size();
+  int nasdaq_returns_length = nasdaq_returns.size();
+  int sp500_returns_length = sp500_returns.size();
   int start_weekday = 0;
   int total_losses = 0;
   int total_wins = 0;
 
   json::reverse_iterator it = all_returns.rbegin();
   json todays_return_json = it.value();
-  json::reverse_iterator qit = qqq_returns.rbegin();
-  json todays_qqq_return_json = qit.value();
+  json::reverse_iterator nasdaq_it = nasdaq_returns.rbegin();
+  json::reverse_iterator sp500_it = sp500_returns.rbegin();
+  json todays_nasdaq_return_json = nasdaq_it.value();
+  json todays_sp500_return_json = sp500_it.value();
 
-  const double DAILY_SALARY = 506.71;
+  const double DAILY_SALARY = 576.92;
   const double todays_close = todays_return_json["close"];
   const double todays_open = todays_return_json["open"];
   const double todays_profit_dollars = todays_close - todays_open;
@@ -71,10 +76,15 @@ void log() {
   const double todays_profit_salary_ratio =
       todays_profit_dollars / DAILY_SALARY;
 
-  const double todays_qqq_close = todays_qqq_return_json["close"];
-  const double todays_qqq_open = todays_qqq_return_json["open"];
-  const double todays_qqq_profit_ratio =
-      (todays_qqq_close - todays_qqq_open) / todays_qqq_open;
+  const double todays_nasdaq_close = todays_nasdaq_return_json["close"];
+  const double todays_nasdaq_open = todays_nasdaq_return_json["open"];
+  const double todays_nasdaq_profit_ratio =
+      (todays_nasdaq_close - todays_nasdaq_open) / todays_nasdaq_open;
+
+  const double todays_sp500_close = todays_sp500_return_json["close"];
+  const double todays_sp500_open = todays_sp500_return_json["open"];
+  const double todays_sp500_profit_ratio =
+      (todays_sp500_close - todays_sp500_open) / todays_sp500_open;
 
   for (; it != all_returns.rend(); it++) {
     const std::tm parsed_date =
@@ -118,15 +128,26 @@ void log() {
     }
   }
 
-  for (; qit != qqq_returns.rend(); qit++) {
-    json return_json = qit.value();
+  for (; nasdaq_it != nasdaq_returns.rend(); nasdaq_it++) {
+    json return_json = nasdaq_it.value();
 
     const double close = return_json["close"];
     const double open = return_json["open"];
     const double profit_dollars = close - open;
     const double profit_ratio = profit_dollars / open;
 
-    avg_qqq_profit_ratio += profit_ratio / qqq_returns_length;
+    avg_nasdaq_profit_ratio += profit_ratio / nasdaq_returns_length;
+  }
+
+  for (; sp500_it != sp500_returns.rend(); sp500_it++) {
+    json return_json = sp500_it.value();
+
+    const double close = return_json["close"];
+    const double open = return_json["open"];
+    const double profit_dollars = close - open;
+    const double profit_ratio = profit_dollars / open;
+
+    avg_sp500_profit_ratio += profit_ratio / sp500_returns_length;
   }
 
   std::cout << fmt.bold << fmt.yellow << std::endl;
@@ -134,18 +155,21 @@ void log() {
          total_wins, consecutive_wins, total_losses, consecutive_losses);
 
   std::cout << fmt.cyan;
-  printf("* Latest Return: %c$%'.2f (%+.2f%%) (%+.2f%% Daily Salary) (vs. "
-         "QQQ: %+.2f%%)\n",
+  printf("* Latest Return: %c$%'.2f (%+.2f%%) (%+.2f%% Daily Salary)\n"
+         "                 (vs. NASDAQ: %+.2f%%) (vs. S&P 500: %+.2f%%)\n",
          sign(todays_profit_dollars), abs(todays_profit_dollars),
          todays_profit_ratio * 100, todays_profit_salary_ratio * 100,
-         todays_qqq_profit_ratio * 100);
+         todays_nasdaq_profit_ratio * 100, todays_sp500_profit_ratio * 100);
 
-  printf("* Week's Return: %c$%'.2f \n", sign(week_total), abs(week_total));
-  printf("* Total Return: %c$%'.2f\n", sign(grand_total), abs(grand_total));
+  printf("* Week's Return: %c$%'.2f • Total Return: %c$%'.2f\n",
+         sign(week_total), abs(week_total), sign(grand_total),
+         abs(grand_total));
 
   std::cout << fmt.yellow;
-  printf("* Average Return: %+.2f%% • Average QQQ Return: %+.2f%%\n",
-         avg_profit_ratio * 100, avg_qqq_profit_ratio * 100);
+  printf("* Average Return: %+.2f%% • Average NASDAQ Return: %+.2f%% • Average "
+         "S&P 500 Return: %+.2f%%\n",
+         avg_profit_ratio * 100, avg_nasdaq_profit_ratio * 100,
+         avg_sp500_profit_ratio * 100);
   std::cout << fmt.reset << std::endl;
 }
 } // namespace Returns
