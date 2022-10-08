303bb641744a5a35c9a42525f493fbfc779b60f7
Vidal Ekechukwu <vekechuk@gmail.com>
2022-10-08 02:16:16 +0000

    🦙 feat: Allow large price deltas at opening

--- cpp/sandbox/src/alpaca/tao_bot/fetch_and_persist_quote.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/fetch_and_persist_quote.cpp
@@ -1,12 +1,13 @@
 #ifndef ALPACA__TAO_BOT_fetch_and_persist_quote
 #define ALPACA__TAO_BOT_fetch_and_persist_quote
 
-#include "current_price.cpp" // current_price
-#include "get_quote.cpp"     // get_quote
-#include "tao_bot.h"         // Alpaca::TaoBot, fmt, quote_t
-#include "write_quotes.cpp"  // write_quotes
-#include <iostream>          // std::cout, std::endl
-#include <math.h>            // abs
+#include "current_price.cpp"  // current_price
+#include "get_quote.cpp"      // get_quote
+#include "lib/utils/time.cpp" // ::utils::time_
+#include "tao_bot.h"          // Alpaca::TaoBot, fmt, quote_t
+#include "write_quotes.cpp"   // write_quotes
+#include <iostream>           // std::cout, std::endl
+#include <math.h>             // abs
 
 void Alpaca::TaoBot::fetch_and_persist_quote() {
   const quote_t new_quote = get_quote();
@@ -15,7 +16,7 @@ void Alpaca::TaoBot::fetch_and_persist_quote() {
   const double delta_ratio =
       (new_quote.price - current_price_) / current_price_;
 
-  if (abs(delta_ratio) >= 0.02) {
+  if (::utils::time_::is_at_least({9, 31}) && abs(delta_ratio) >= 0.02) {
     std::cout << fmt.bold << fmt.yellow;
     puts("🧐 STRANGE PRICE MOVEMENT");
     printf("Last Quote: %.2f • New Quote: %.2f\n", current_price_,
