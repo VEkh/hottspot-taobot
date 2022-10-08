37f334f98232d087db9295f933935de7b9d9638d
Vidal Ekechukwu <vekechuk@gmail.com>
2022-10-08 05:36:57 +0000

    💱 feat: Shorten time-eased take profit half life

--- cpp/sandbox/src/alpaca/tao_bot/target_position_profit.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/target_position_profit.cpp
@@ -4,7 +4,6 @@
 #include "dynamic_one_sec_variance.cpp" // dynamic_one_sec_variance
 #include "lib/utils/float.cpp"          // ::utils::float_
 #include "tao_bot.h"                    // Alpaca::TaoBot
-#include <algorithm>                    // std::max
 #include <ctime>                        // std::time
 
 double Alpaca::TaoBot::target_position_profit() {
@@ -15,7 +14,7 @@ double Alpaca::TaoBot::target_position_profit() {
   const double max_profit_to_price_movement_ratio =
       this->open_order_ptr->max_profit / static_one_sec_variance;
 
-  const double max = std::max(min, max_profit_to_price_movement_ratio) + 25;
+  const double max = max_profit_to_price_movement_ratio + 25;
 
   const int duration =
       std::time(nullptr) - this->open_order_ptr->max_profit_timestamp;
--- cpp/sandbox/src/oanda/tao_bot/target_position_profit.cpp
+++ cpp/sandbox/src/oanda/tao_bot/target_position_profit.cpp
@@ -4,23 +4,22 @@
 #include "dynamic_one_sec_variance.cpp" // dynamic_one_sec_variance
 #include "lib/utils/float.cpp"          // ::utils::float_
 #include "tao_bot.h"                    // Oanda::TaoBot
-#include <algorithm>                    // std::max
 #include <ctime>                        // std::time
 
 double Oanda::TaoBot::target_position_profit() {
   const double static_one_sec_variance =
       this->price_movement.three_minute_one_second_variance.average;
 
-  const double min = 40;
+  const double min = 0;
   const double max_profit_to_price_movement_ratio =
       this->open_order_ptr->max_profit / static_one_sec_variance;
 
-  const double max = std::max(min, max_profit_to_price_movement_ratio) + 50;
+  const double max = max_profit_to_price_movement_ratio + 50;
 
   const int duration =
       std::time(nullptr) - this->open_order_ptr->max_profit_timestamp;
 
-  return ::utils::float_::sigmoid(max, min, duration, -0.0015, 1800) *
+  return ::utils::float_::sigmoid(max, min, duration, -0.005, 2.5 * 60) *
          dynamic_one_sec_variance();
 }
 
