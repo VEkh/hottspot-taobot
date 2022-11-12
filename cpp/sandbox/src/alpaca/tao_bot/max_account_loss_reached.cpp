e08302aeacab1c225d6a7e05c52582b70c5c3000
Vidal Ekechukwu <vekechuk@gmail.com>
2022-11-12 02:46:18 +0000

    🦙 feat: ⚡ Stop (again) closing on max account loss
    * I've seen too many instances of big losses rallying to gains. Also,
    the big losses still haven't exceeded my biggest wins, so it seems like
    the mega wins will still exceed the mega losses.

---
+++ cpp/sandbox/src/alpaca/tao_bot/max_account_loss_reached.cpp
@@ -0,0 +1,16 @@
+#ifndef ALPACA__TAO_BOT_max_account_loss_reached
+#define ALPACA__TAO_BOT_max_account_loss_reached
+
+#include "tao_bot.h" // Alpaca::TaoBot
+
+bool Alpaca::TaoBot::max_account_loss_reached() {
+  return false;
+
+  const double loss_ratio =
+      (this->account_balance.balance - this->account_balance.original_balance) /
+      this->account_balance.original_balance;
+
+  return loss_ratio <= this->MAX_ACCOUNT_LOSS_RATIO;
+}
+
+#endif
