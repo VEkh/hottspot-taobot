e23f6dd3f054715fe5a0003f8b13b2ce2cb0edb9
Vidal Ekechukwu <vekechuk@gmail.com>
2022-12-19 16:41:18 +0000

    feat: Positions don't close at price range ratio

--- cpp/sandbox/src/alpaca/tao_bot/build_exit_prices.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/build_exit_prices.cpp
@@ -1,22 +1,16 @@
 #ifndef ALPACA__TAO_BOT_build_exit_prices
 #define ALPACA__TAO_BOT_build_exit_prices
 
-#include "get_quote_price_range.cpp" // get_quote_price_range
-#include "tao_bot.h"                 // Alpaca::TaoBot
-#include <algorithm>                 // std::min
-#include <math.h>                    // abs
-#include <utility>                   // std::pair
+#include "tao_bot.h" // Alpaca::TaoBot
+#include <math.h>    // abs
 
 Alpaca::TaoBot::exit_prices_t Alpaca::TaoBot::build_exit_prices() {
   const double static_one_sec_variance =
       this->price_movement.three_minute_one_second_variance.average;
 
-  const std::pair<double, double> quote_range = get_quote_price_range();
-  const double price_range = quote_range.first - quote_range.second;
   const int max_loss_coefficient = -40;
 
-  const double max_loss = std::min(
-      max_loss_coefficient * static_one_sec_variance, price_range / -4.0);
+  const double max_loss = max_loss_coefficient * static_one_sec_variance;
   const double min_profit = abs(max_loss);
 
   const double trailing_stop_profit = this->open_order_ptr->max_profit * 0.5;
