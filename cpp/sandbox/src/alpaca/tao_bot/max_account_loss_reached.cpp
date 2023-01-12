a9d25a6a9a649e8b4a0b335b2ddf3f6e4e72d5a8
Vidal Ekechukwu <vekechuk@gmail.com>
2023-01-12 20:56:44 +0000

    🦙 feat: Re-introduce hard max stop loss

--- cpp/sandbox/src/alpaca/tao_bot/max_account_loss_reached.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/max_account_loss_reached.cpp
@@ -1,20 +1,15 @@
 #ifndef ALPACA__TAO_BOT_max_account_loss_reached
 #define ALPACA__TAO_BOT_max_account_loss_reached
 
-#include "batch_volatility.cpp"          // batch_volatility
 #include "build_account_exit_prices.cpp" // build_account_exit_prices
 #include "tao_bot.h"                     // Alpaca::TaoBot
 
 bool Alpaca::TaoBot::max_account_loss_reached() {
   const account_exit_prices_t exit_prices_ = build_account_exit_prices();
 
-  return false;
-
-  if (batch_volatility() < 1.0) {
-    return false;
-  }
-
-  return this->account_balance.balance <= exit_prices_.session_stop_loss;
+  return (exit_prices_.current_profit /
+          this->account_balance.original_balance) <=
+         this->MAX_ACCOUNT_LOSS_RATIO;
 }
 
 #endif
