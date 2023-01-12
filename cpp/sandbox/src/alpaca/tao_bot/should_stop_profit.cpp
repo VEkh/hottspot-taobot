de7ab6c8a56bd2845e7ca4240ded054e070ac918
Vidal Ekechukwu <vekechuk@gmail.com>
2023-01-12 21:28:25 +0000

    🦙 feat: Only production stops at superprofit
    * Staging continues to experiment with volatility-gated re-entry.

--- cpp/sandbox/src/alpaca/tao_bot/should_stop_profit.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/should_stop_profit.cpp
@@ -1,11 +1,9 @@
 #ifndef ALPACA__TAO_BOT_should_stop_profit
 #define ALPACA__TAO_BOT_should_stop_profit
 
-#include "batch_volatility.cpp"          // batch_volatility
 #include "build_account_exit_prices.cpp" // build_account_exit_prices
 #include "has_super_profited.cpp"        // has_super_profited
-#include "tao_bot.h"      // Alpaca::TaoBot, account_exit_prices_t
-#include "volatility.cpp" // volatility
+#include "tao_bot.h" // Alpaca::TaoBot, account_exit_prices_t
 
 bool Alpaca::TaoBot::should_stop_profit() {
   const account_exit_prices_t exit_prices_ = build_account_exit_prices();
@@ -14,17 +12,14 @@ bool Alpaca::TaoBot::should_stop_profit() {
       exit_prices_.max_profit >= exit_prices_.target_max_profit &&
       exit_prices_.current_profit <= exit_prices_.session_stop_profit_loss;
 
-  if (has_super_profited() && !is_session_profit_slipping) {
+  if (this->api_client.is_live() && has_super_profited() &&
+      !is_session_profit_slipping) {
     const double target_profit_cash = this->account_balance.original_balance *
                                       this->TARGET_ACCOUNT_MIN_PROFIT_RATIO;
 
     return exit_prices_.current_profit > target_profit_cash &&
            exit_prices_.current_profit <=
                (2.0 / 3) * exit_prices_.overall_max_profit;
-  }
-
-  if (batch_volatility() < 1.0) {
-    return false;
   }
 
   return is_session_profit_slipping;
