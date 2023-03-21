07feecc93cea7da0fe7a198e74fd19449b12cb7f
Vidal Ekechukwu <vekechuk@gmail.com>
2023-03-21 19:14:03 +0000

    🦙 feat: Don't log quote and current timestamp diff

--- cpp/sandbox/src/alpaca/client/parse_quote.cpp
+++ cpp/sandbox/src/alpaca/client/parse_quote.cpp
@@ -13,19 +13,10 @@ Alpaca::Client::parse_quote(const std::string &response_body) {
   const double ask = quote_json["quote"]["ap"];
   const double bid = quote_json["quote"]["bp"];
   const double price = (ask + bid) * 0.5;
-  const std::string timestamp_str = quote_json["quote"]["t"];
-  const double timestamp =
-      ::utils::time_::quote_timestamp_to_epoch_double(timestamp_str.c_str());
 
   // The quote's timestamp at "t" is sometimes wildly inaccurate, so you'll
-  // have to settle for the system's current time for now.
+  // have to settle for the system's current time.
   const double now = ::utils::time_::epoch("nanoseconds") / 1.0e9;
-
-  if (abs((int)now - (int)timestamp) > 1) {
-    std::cout << fmt.bold << fmt.yellow;
-    printf("Now: %.9f • Parsed Epoch: %.9f\n", now, timestamp);
-    std::cout << fmt.reset;
-  }
 
   quote_t quote;
   quote.ask = ask;
