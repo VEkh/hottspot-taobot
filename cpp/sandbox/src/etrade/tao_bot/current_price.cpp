4362b946562e73843a49d5c8ce8b06a460a7f761
Vidal Ekechukwu <vekechuk@gmail.com>
2023-04-03 18:15:51 +0000

    chore: Remove remainder of ETrade code 💀🙏🏾

--- cpp/sandbox/src/etrade/tao_bot/current_price.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef ETRADE__TAO_BOT_current_price
-#define ETRADE__TAO_BOT_current_price
-
-#include "tao_bot.h" // ETrade::TaoBot
-
-double ETrade::TaoBot::current_price() { return this->quotes.back().price; }
-
-#endif
