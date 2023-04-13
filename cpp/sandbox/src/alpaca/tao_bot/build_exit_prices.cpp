54a4f7316bcb2940341c0b7178687a7f6aa84704
Vidal Ekechukwu <vekechuk@gmail.com>
2023-04-13 21:54:45 +0000

    🦙 feat: Take profit varies with TaoBot environment

--- cpp/sandbox/src/alpaca/tao_bot/build_exit_prices.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/build_exit_prices.cpp
@@ -2,23 +2,35 @@
 #define ALPACA__TAO_BOT_build_exit_prices
 
 #include "tao_bot.h" // Alpaca::TaoBot
+#include <map>       // std::map
 #include <math.h>    // abs
+#include <string>    // std::string
 
 Alpaca::TaoBot::exit_prices_t Alpaca::TaoBot::build_exit_prices() {
   const double static_one_sec_variance = this->one_sec_variance_avgs.running;
 
   const int max_loss_coefficient = -62.5;
   const double trailing_stop_profit_ratio = 1 / 1.1;
+  double stop_profit_stop_loss_ratio = 2.5;
+
+  if (this->backtest.is_active) {
+    std::map<std::string, double> ratios = {
+        {"backtest_no_stop_profit", 99999.0},
+        {"backtest_1_5", 1.5},
+        {"backtest_2_0", 2.0},
+        {"backtest_2_25", 2.25},
+        {"backtest_2_5", 2.5},
+        {"backtest_2_75", 2.75},
+        {"backtest_3_0", 3},
+    };
+
+    const double ratio = ratios[this->backtest.config.api_key_id];
+
+    stop_profit_stop_loss_ratio = ratio ? ratio : stop_profit_stop_loss_ratio;
+  }
 
   const double max_loss = max_loss_coefficient * static_one_sec_variance;
-  double stop_profit = abs(2.5 * max_loss);
-
-  if (this->api_client.config.is_live) {
-    const double computed_stop_profit =
-        this->quoter.db_quote.get_stop_profit(this->symbol);
-
-    stop_profit = computed_stop_profit ? computed_stop_profit : stop_profit;
-  }
+  const double stop_profit = abs(stop_profit_stop_loss_ratio * max_loss);
 
   const double min_profit = stop_profit / trailing_stop_profit_ratio;
 
