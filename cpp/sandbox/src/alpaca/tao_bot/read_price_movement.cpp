0bb1517b18337321ade49d9f7f284c900b3465dd
Vidal Ekechukwu <vekechuk@gmail.com>
2023-05-15 21:09:33 +0000

    🦙 feat: 🧪  Filter price movement with time range

--- cpp/sandbox/src/alpaca/tao_bot/read_price_movement.cpp
+++ cpp/sandbox/src/alpaca/tao_bot/read_price_movement.cpp
@@ -3,15 +3,30 @@
 
 #include "tao_bot.h" // Alpaca::TaoBot
 
+#include <regex>  // std::regex, std::smatch
+#include <string> // std;:string, std::stoi
+
 void Alpaca::TaoBot::read_price_movement() {
   if (!this->backtest.should_exec_slow_query(this->current_epoch)) {
     return;
   }
 
+  int timestamp_lower_bound_interval_days = 45;
+
+  if (this->backtest.is_active) {
+    std::smatch match;
+    const std::string id = this->backtest.config.api_key_id;
+
+    if (std::regex_search(id, match, std::regex("-one_sec_(.+)$"))) {
+      timestamp_lower_bound_interval_days = std::stoi(match[1].str());
+    }
+  }
 
   this->avg_one_sec_variances =
       this->quoter.db_quote.get_avg_one_sec_variances({
           .symbol = this->symbol,
+          .timestamp_lower_bound_interval_days =
+              timestamp_lower_bound_interval_days,
           .timestamp_upper_bound = this->current_epoch,
       });
 }
--- cpp/sandbox/src/db/quote/get_avg_one_sec_variances.cpp
+++ cpp/sandbox/src/db/quote/get_avg_one_sec_variances.cpp
@@ -12,6 +12,8 @@ DB::Quote::avg_one_sec_variances_t DB::Quote::get_avg_one_sec_variances(
     const get_avg_one_sec_variances_args_t args) {
   const bool debug = args.debug;
   const double timestamp_upper_bound = args.timestamp_upper_bound;
+  const int timestamp_lower_bound_interval_days =
+      args.timestamp_lower_bound_interval_days;
   const std::string symbol = args.symbol;
 
   const char *query_format = R"(
@@ -33,30 +35,27 @@ DB::Quote::avg_one_sec_variances_t DB::Quote::get_avg_one_sec_variances(
       limit 1) as latest_avg
       join lateral (
         select
-          avg(limited.avg_one_sec_variance) as avg
-        from (
-          select
-            avg_one_sec_variance
-          from
-            avg_one_sec_variances
-          where
-            symbol = latest_avg.symbol
-            and timestamp <= to_timestamp(%f)
-          order by
-            timestamp desc
-          limit 500000) as limited) as limited_agg on true;
+          avg(avg_one_sec_variance)
+        from
+          avg_one_sec_variances
+        where
+          symbol = latest_avg.symbol
+          and timestamp >= to_timestamp(%f) - '%i days'::interval
+          and timestamp <= to_timestamp(%f)) as limited_agg on true
   )";
 
   char *sanitized_symbol =
       PQescapeLiteral(this->conn.conn, symbol.c_str(), symbol.size());
 
-  const size_t query_l = strlen(query_format) + strlen(sanitized_symbol) +
-                         std::to_string(timestamp_upper_bound).size() +
-                         std::to_string(timestamp_upper_bound).size();
+  const size_t query_l =
+      strlen(query_format) + strlen(sanitized_symbol) +
+      3 * (std::to_string(timestamp_upper_bound).size()) +
+      std::to_string(timestamp_lower_bound_interval_days).size();
 
   char query[query_l];
   snprintf(query, query_l, query_format, sanitized_symbol,
-           timestamp_upper_bound, timestamp_upper_bound);
+           timestamp_upper_bound, timestamp_upper_bound,
+           timestamp_lower_bound_interval_days, timestamp_upper_bound);
 
   query_result_t result = this->conn.exec(query, args.debug);
 
@@ -64,63 +63,5 @@ DB::Quote::avg_one_sec_variances_t DB::Quote::get_avg_one_sec_variances(
 
   return result_to_avg_one_sec_variances(result);
 }
-
-// TODO: Figure out why this is so catastrophic at 7 days. It would mean the
-// stop loss threshold would have to change.
-// DB::Quote::avg_one_sec_variances_t DB::Quote::get_avg_one_sec_variances(
-//     const get_avg_one_sec_variances_args_t args) {
-//   const bool debug = args.debug;
-//   const double timestamp_upper_bound = args.timestamp_upper_bound;
-//   const int timestamp_lower_bound_interval_days =
-//       args.timestamp_lower_bound_interval_days;
-//   const std::string symbol = args.symbol;
-
-//   const char *query_format = R"(
-//     select
-//       latest_avg.avg_one_sec_variance as latest,
-//       limited_agg.avg as running,
-//       latest_avg.symbol
-//     from (
-//       select
-//         avg_one_sec_variance,
-//         symbol
-//       from
-//         avg_one_sec_variances
-//       where
-//         symbol = %s
-//         and timestamp <= to_timestamp(%f)
-//       order by
-//         timestamp desc
-//       limit 1) as latest_avg
-//       join lateral (
-//         select
-//           avg(avg_one_sec_variance)
-//         from
-//           avg_one_sec_variances
-//         where
-//           symbol = latest_avg.symbol
-//           and timestamp >= to_timestamp(%f) - '%i days'::interval
-//           and timestamp <= to_timestamp(%f)) as limited_agg on true
-//   )";
-
-//   char *sanitized_symbol =
-//       PQescapeLiteral(this->conn.conn, symbol.c_str(), symbol.size());
-
-//   const size_t query_l =
-//       strlen(query_format) + strlen(sanitized_symbol) +
-//       3 * (std::to_string(timestamp_upper_bound).size()) +
-//       std::to_string(timestamp_lower_bound_interval_days).size();
-
-//   char query[query_l];
-//   snprintf(query, query_l, query_format, sanitized_symbol,
-//            timestamp_upper_bound, timestamp_upper_bound,
-//            timestamp_lower_bound_interval_days, timestamp_upper_bound);
-
-//   query_result_t result = this->conn.exec(query, args.debug);
-
-//   PQfreemem(sanitized_symbol);
-
-//   return result_to_avg_one_sec_variances(result);
-// }
 
 #endif
