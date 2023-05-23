f206211e27879737400aaad0f621a8a8cc5329bd
Vidal Ekechukwu <vekechuk@gmail.com>
2023-05-23 08:13:17 +0000

    feat: Deploy db init and migrations

--- cpp/sandbox/sandbox.cpp
+++ cpp/sandbox/sandbox.cpp
@@ -4,21 +4,12 @@
 
 #include "db/quote/quote.cpp" // DB::Quote
 #include "lib/pg/pg.cpp"      // Pg
+
 int main(int argc, char *argv[]) {
   Pg pg;
   pg.connect();
 
   DB::Quote db_quote(pg);
-
-  db_quote.upsert_all_avg_one_sec_variances({
-      .starting_from = "",
-      .symbol = "amzn",
-  });
-
-  db_quote.upsert_all_avg_one_sec_variances({
-      .starting_from = "",
-      .symbol = "tsla",
-  });
 
   pg.disconnect();
 }
