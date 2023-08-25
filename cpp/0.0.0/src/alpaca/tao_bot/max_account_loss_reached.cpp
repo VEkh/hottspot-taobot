b170a725a0c17c4074edba9f5ab1ef83cadf2e50
Vidal Ekechukwu <vekechuk@gmail.com>
2023-08-25 18:28:16 +0000

    🦙 feat: Emergency brake at max, not current loss

--- cpp/0.0.0/src/alpaca/tao_bot/max_account_loss_reached.cpp
+++ cpp/0.0.0/src/alpaca/tao_bot/max_account_loss_reached.cpp
@@ -10,15 +10,17 @@
 bool Alpaca::TaoBot::max_account_loss_reached() {
   const account_exit_prices_t exit_prices_ = build_account_exit_prices();
 
-  const double current_profit_ratio =
-      exit_prices_.current_profit / this->account_snapshot.original_equity;
+  const double current_max_loss_ratio =
+      (this->account_snapshot.min_equity -
+       this->account_snapshot.original_equity) /
+      this->account_snapshot.original_equity;
 
   if (this->backtest.is_active &&
       !this->backtest.config.account_max_stop_loss) {
     return false;
   }
 
-  if (current_profit_ratio <= this->MEGA_MAX_ACCOUNT_LOSS_RATIO) {
+  if (current_max_loss_ratio <= this->MEGA_MAX_ACCOUNT_LOSS_RATIO) {
     return true;
   }
 
@@ -45,7 +47,7 @@ bool Alpaca::TaoBot::max_account_loss_reached() {
     loss_ratio = -1 * (std::stod(ratio_string) / 100.0);
   }
 
-  return current_profit_ratio <= loss_ratio;
+  return current_max_loss_ratio <= loss_ratio;
 }
 
 #endif
