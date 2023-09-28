84ada845473d57751a81b49a0109ffbece811f41
Vidal Ekechukwu <vekechuk@gmail.com>
2023-09-28 23:39:59 +0000

    fix: 💿 Most recent candle sometimes not built

--- sandbox/src/db/five_min_candle/build.cpp
+++ sandbox/src/db/five_min_candle/build.cpp
@@ -10,6 +10,7 @@
 #include "upsert.cpp"            // upsert
 #include <algorithm>             // std::max, std::min
 #include <iostream>              // std::cout, std::endl
+#include <iterator>              // std::next
 #include <list>                  // std::list
 #include <math.h>                // INFINITY
 #include <stdio.h>               // printf
@@ -31,11 +32,16 @@ void DB::FiveMinCandle::build() {
   int quotes_count = 1;
   std::list<quote_t>::const_iterator quote = latest_quotes.begin();
 
+  std::cout << fmt.bold << fmt.cyan;
+  printf("💲 %i quotes loaded\n", (int)latest_quotes.size());
+  std::cout << fmt.reset << std::endl;
+
   for (; quote != latest_quotes.end(); quote++, quotes_count++) {
     const candle_bounds_t bounds = quote_to_bounds(*quote);
     const double mid = quote->mid();
 
-    if (candle.closed_at && quote->timestamp > candle.closed_at) {
+    if (candle.closed_at && (quote->timestamp > candle.closed_at ||
+                             std::next(quote) == latest_quotes.end())) {
       upsert(candle);
       candle = candle_t();
       candles_count++;
@@ -68,7 +74,7 @@ void DB::FiveMinCandle::build() {
   const long int duration = time(nullptr) - clock_start;
 
   std::cout << fmt.bold << fmt.green;
-  printf("🎉 Successfully built candles in %s\n",
+  printf("🎉 Successfully built %i candle(s) in %s\n", candles_count,
          ::utils::integer_::seconds_to_clock(duration).c_str());
   std::cout << fmt.reset;
 }
