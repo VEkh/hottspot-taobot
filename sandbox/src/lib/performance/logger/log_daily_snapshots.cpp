4e45cf02e32c0cbd7364d1727b46e47a0cae462d
Vidal Ekechukwu <vekechuk@gmail.com>
2023-11-19 15:03:09 +0000

    📊 feat: Log # days target reached

--- sandbox/src/lib/performance/logger/log_daily_snapshots.cpp
+++ sandbox/src/lib/performance/logger/log_daily_snapshots.cpp
@@ -3,6 +3,7 @@
 
 #include "lib/formatted.cpp"   // Formatted
 #include "lib/utils/float.cpp" // ::utils::float_
+#include "lib/utils/map.cpp"   // ::utils::map
 #include "lib/utils/time.cpp"  // ::utils::time_
 #include "logger.h"            // Performance::Logger, account_snapshot_t
 #include <iostream>            // std::cout, std::endl
@@ -11,18 +12,53 @@
 #include <map>                 // std::map
 #include <math.h>              // abs
 #include <stdio.h>             // printf
-#include <string>              // std::stoi, std::string
+#include <string>              // std::stod, std::stoi, std::string
+
+struct should_increment_target_reached_count_args_t {
+  double max_equity_timestamp;
+  double max_profit_percent;
+  double min_equity_timestamp;
+  double min_profit_percent;
+  double target_profit_percent;
+};
+
+bool should_increment_target_reached_count(
+    const should_increment_target_reached_count_args_t args) {
+  const double max_equity_timestamp = args.max_equity_timestamp;
+  const double max_profit_percent = args.max_profit_percent;
+  const double min_equity_timestamp = args.min_equity_timestamp;
+  const double min_profit_percent = args.min_profit_percent;
+  const double target_profit_percent = args.target_profit_percent;
+
+  if (max_profit_percent < target_profit_percent) {
+    return false;
+  }
+
+  if (abs(min_profit_percent) < target_profit_percent) {
+    return true;
+  }
+
+  return max_equity_timestamp < min_equity_timestamp;
+}
 
 void Performance::Logger::log_daily_snapshots(
     const log_daily_snapshots_args_t args) {
-  const std::string api_key_id = args.api_key_id;
-  std::map<std::string, std::string> flags = args.flags;
+  std::map<std::string, std::string> default_flags = {
+      {"debug", "0"},
+      {"target", "2.0"},
+  };
 
-  const bool debug = flags["debug"].empty() ? false : std::stoi(flags["debug"]);
+  const std::string api_key_id = args.api_key_id;
+  std::map<std::string, std::string> flags =
+      ::utils::map::merge(default_flags, args.flags);
+
+  const bool debug = std::stoi(flags["debug"]);
+  const double target_profit_percent = std::stod(flags["target"]);
 
   double daily_dollars = 0.0;
   double daily_ratio = 0.0;
-  int count = 0;
+  int day_count = 0;
+  int target_reached_day_count = 0;
 
   setlocale(LC_NUMERIC, "");
   Formatted::fmt_stream_t fmt = Formatted::stream();
@@ -43,7 +79,7 @@ void Performance::Logger::log_daily_snapshots(
   }
 
   for (std::list<account_snapshot_t>::iterator it = snapshots.begin();
-       it != snapshots.end(); it++, count++) {
+       it != snapshots.end(); it++, day_count++) {
     const account_snapshot_t snapshot = *it;
 
     const double current_equity = snapshot.equity;
@@ -65,6 +101,13 @@ void Performance::Logger::log_daily_snapshots(
 
     daily_dollars += current_profit;
     daily_ratio += current_profit / original_equity;
+    target_reached_day_count += (int)(should_increment_target_reached_count({
+        .max_equity_timestamp = max_equity_timestamp,
+        .max_profit_percent = max_profit_percent,
+        .min_equity_timestamp = min_equity_timestamp,
+        .min_profit_percent = min_profit_percent,
+        .target_profit_percent = target_profit_percent,
+    }));
 
     Formatted::Stream profit_color = current_profit >= 0 ? fmt.green : fmt.red;
 
@@ -75,27 +118,27 @@ void Performance::Logger::log_daily_snapshots(
     std::cout << snapshot_time;
     std::cout << fmt.no_underline << profit_color << std::endl;
 
-    printf("Current Equity:          $%'.2f (%+'.2f) (%+'.2f%%)\n",
+    printf("Current Equity:             $%'.2f (%+'.2f) (%+'.2f%%)\n",
            current_equity, current_profit, current_profit_percent);
-    printf("Max Equity:              $%'.2f (%+'.2f) (%+'.2f%%) @ %s\n",
+    printf("Max Equity:                 $%'.2f (%+'.2f) (%+'.2f%%) @ %s\n",
            max_equity, max_profit, max_profit_percent,
            ::utils::time_::date_string(max_equity_timestamp, "%H:%M %Z",
                                        "America/Chicago")
                .c_str());
-    printf("Min Equity:              $%'.2f (%+'.2f) (%+'.2f%%) @ %s\n",
+    printf("Min Equity:                 $%'.2f (%+'.2f) (%+'.2f%%) @ %s\n",
            min_equity, min_profit, min_profit_percent,
            ::utils::time_::date_string(min_equity_timestamp, "%H:%M %Z",
                                        "America/Chicago")
                .c_str());
-    printf("Original Equity:         $%'.2f\n", original_equity);
+    printf("Original Equity:            $%'.2f\n", original_equity);
   }
 
   std::cout << fmt.bold << fmt.cyan << fmt.underline << std ::endl;
   std::cout << "📊 Stats\n";
   std::cout << fmt.reset;
 
-  const double avg_daily_dollars = daily_dollars / count;
-  const double avg_daily_ratio = daily_ratio / count;
+  const double avg_daily_dollars = daily_dollars / day_count;
+  const double avg_daily_ratio = daily_ratio / day_count;
 
   Formatted::Stream avg_daily_dollars_color =
       avg_daily_dollars < 0 ? fmt.red : fmt.green;
@@ -104,15 +147,22 @@ void Performance::Logger::log_daily_snapshots(
   Formatted::Stream total_color = daily_dollars < 0 ? fmt.red : fmt.green;
 
   std::cout << fmt.bold << avg_daily_dollars_color;
-  printf("Avg Daily Return:        %c$%'.2f\n",
+  printf("Avg Daily Return:           %c$%'.2f\n",
          ::utils::float_::sign_char(avg_daily_dollars), abs(avg_daily_dollars));
 
   std::cout << fmt.bold << avg_daily_ratio_color;
-  printf("Avg Daily %% Return:      %+'.2f%%\n", avg_daily_ratio * 100.0);
+  printf("Avg Daily %% Return:         %+'.2f%%\n", avg_daily_ratio * 100.0);
 
   std::cout << fmt.bold << total_color;
-  printf("Total Return:            %c$%'.2f\n",
+  printf("Total Return:               %c$%'.2f\n",
          ::utils::float_::sign_char(daily_dollars), abs(daily_dollars));
+
+  std::cout << std::endl;
+
+  std::cout << fmt.bold << fmt.yellow;
+  printf("Days %.2f%% Target Reached:  %i/%i (%.2f%%)\n", target_profit_percent,
+         target_reached_day_count, day_count,
+         100.0 * target_reached_day_count / day_count);
   std::cout << fmt.reset << fmt.cyan;
 }
 
