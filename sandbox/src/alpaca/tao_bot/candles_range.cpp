8743d5ab137d5375cfcddd0bf5dd28e96905a187
Vidal Ekechukwu <vekechuk@gmail.com>
2024-01-25 18:57:47 +0000

    🦙 feat: Opens/Closes define consolidation
    * Not highs and low

--- sandbox/src/alpaca/tao_bot/candles_range.cpp
+++ sandbox/src/alpaca/tao_bot/candles_range.cpp
@@ -26,17 +26,11 @@ Alpaca::TaoBot::range_t Alpaca::TaoBot::candles_range(const int start_index,
   range.opened_at = it->opened_at;
 
   for (; it != this->latest_candles.end() && counter < count; it++, counter++) {
-    // TODO: Decide
     const std::valarray<double> highs = {range.high, it->close, it->open};
     const std::valarray<double> lows = {range.low, it->close, it->open};
 
     range.high = highs.max();
     range.low = lows.min();
-
-    // TODO: Decide
-    // range.high = std::max(range.high, it->high);
-    // range.low = std::min(range.low, it->low);
-
     range.closed_at = it->closed_at;
   }
 
