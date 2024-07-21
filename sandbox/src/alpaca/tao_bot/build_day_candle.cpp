8e1f07d0c4ff9dbf398076bb56f411c6ac37640d
Vidal Ekechukwu <vekechuk@gmail.com>
2024-07-21 04:05:58 +0000

    🦙 feat: Day candle only uses candles for values

--- sandbox/src/alpaca/tao_bot/build_day_candle.cpp
+++ sandbox/src/alpaca/tao_bot/build_day_candle.cpp
@@ -1,17 +1,15 @@
 #ifndef ALPACA__TAO_BOT_build_day_candle
 #define ALPACA__TAO_BOT_build_day_candle
 
-#include "current_mid.cpp" // current_mid
-#include "tao_bot.h"       // Alpaca::TaoBot, candle_t
-#include <valarray>        // std::valarray
+#include "tao_bot.h" // Alpaca::TaoBot, candle_t
+#include <valarray>  // std::valarray
 
 void Alpaca::TaoBot::build_day_candle() {
-  const double mid = current_mid();
-
-  this->day_candle.close = mid;
   this->day_candle.closed_at = this->current_epoch;
 
   for (const candle_t candle : this->latest_candles) {
+    this->day_candle.close = this->day_candle.close;
+
     this->day_candle.open =
         this->day_candle.open ? this->day_candle.open : candle.open;
 
@@ -19,8 +17,8 @@ void Alpaca::TaoBot::build_day_candle() {
                                      ? this->day_candle.opened_at
                                      : candle.opened_at;
 
-    std::valarray<double> highs = {this->day_candle.high, candle.high, mid};
-    std::valarray<double> lows = {this->day_candle.low, candle.low, mid};
+    std::valarray<double> highs = {this->day_candle.high, candle.high};
+    std::valarray<double> lows = {this->day_candle.low, candle.low};
 
     this->day_candle.high = highs.max();
     this->day_candle.low = lows.min();
