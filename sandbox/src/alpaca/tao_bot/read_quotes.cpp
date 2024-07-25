67dce009736b535491f4810ed0aee1249ffdf303
Vidal Ekechukwu <vekechuk@gmail.com>
2024-07-25 03:47:05 +0000

    🐞⌛ fix: Can't advance through unskipped holidays

--- sandbox/src/alpaca/tao_bot/read_quotes.cpp
+++ sandbox/src/alpaca/tao_bot/read_quotes.cpp
@@ -1,10 +1,10 @@
 #ifndef ALPACA__TAO_BOT_read_quotes
 #define ALPACA__TAO_BOT_read_quotes
 
-#include "lib/formatted.cpp" // Formatted
-#include "tao_bot.h"         // Alpaca::TaoBot, quote_t
-#include <list>              // std::list
-#include <string>            // std::string
+#include "lib/utils/time.cpp" // ::utils::time_
+#include "tao_bot.h"          // Alpaca::TaoBot, quote_t
+#include <list>               // std::list
+#include <string>             // std::string
 
 void Alpaca::TaoBot::read_quotes() {
   double end_at = this->current_epoch;
@@ -14,9 +14,9 @@ void Alpaca::TaoBot::read_quotes() {
 
   if (this->backtest.is_active) {
     end_at = this->market_close_epoch;
+    start_at = this->current_epoch;
     limit = 0;
     sort_direction = "asc";
-    start_at = this->current_epoch;
   }
 
   const std::list<quote_t> quotes_ = this->quoter.db_quote.get_last({
@@ -29,12 +29,27 @@ void Alpaca::TaoBot::read_quotes() {
   });
 
   if (quotes_.empty()) {
-    const std::string error_message = Formatted::error_message(
-        "No available quotes for " + this->symbol + " in the database.");
+    std::cout << fmt.bold << fmt.red;
+    printf("No available quotes for %s in the database @ %s\n",
+           this->symbol.c_str(),
+           ::utils::time_::date_string(this->current_epoch, "%F %R CT",
+                                       "America/Chicago")
+               .c_str());
 
-    printf("%s @ %f\n", error_message.c_str(), this->current_epoch);
+    std::cout << fmt.yellow;
+    printf("⌛ Advancing epoch...\n");
+    std::cout << fmt.reset << std::endl;
 
-    return read_quotes();
+    if (this->backtest.is_active) {
+      advance_current_epoch(this->current_epoch + 1);
+    } else {
+      advance_current_epoch();
+    }
+
+    return this->market_availability.is_end_of_trading_period(
+               this->current_epoch)
+               ? void()
+               : read_quotes();
   }
 
   this->quotes = quotes_;
