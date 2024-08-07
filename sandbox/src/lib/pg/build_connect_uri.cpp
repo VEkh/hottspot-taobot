3ec008060efadb4ae884b5a9034280c99112010f
Vidal Ekechukwu <vekechuk@gmail.com>
2024-08-07 06:34:25 +0000

    🐞 fix: en_US.UTF-8 locale breaks DB connection

--- sandbox/src/lib/pg/build_connect_uri.cpp
+++ sandbox/src/lib/pg/build_connect_uri.cpp
@@ -1,18 +1,26 @@
 #ifndef PG_build_connect_uri
 #define PG_build_connect_uri
 
-#include "pg.h"    // Pg
-#include <sstream> // std::stringstream
-#include <string>  // std::string
+#include "pg.h"     // Pg
+#include <stdio.h>  // snprintf
+#include <string.h> // strlen
+#include <string>   // std::string, std::to_string
 
 std::string Pg::build_connect_uri() {
-  std::stringstream out;
+  const char *format = "postgres://%s:%s@%s:%i/%s";
 
-  out << "postgres://" << this->config.username << ":" << this->config.password
-      << "@" << this->config.host << ":" << this->config.port << "/"
-      << this->config.name;
+  const size_t size = strlen(format) + this->config.username.size() +
+                      this->config.password.size() + this->config.host.size() +
+                      std::to_string(this->config.port).size() +
+                      this->config.name.size();
 
-  return out.str();
+  char uri[size];
+
+  snprintf(uri, size, format, this->config.username.c_str(),
+           this->config.password.c_str(), this->config.host.c_str(),
+           this->config.port, this->config.name.c_str());
+
+  return uri;
 }
 
 #endif
